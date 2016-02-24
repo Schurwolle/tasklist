01dfa320a252ea4dd67996187df8962808e0e738
Schurwolle <cartmen023@hotmail.com>
2016-02-24 11:38:57 +0100

    first commit

---
+++ resources/views/errors/list.blade.php
@@ -0,0 +1,8 @@
+@if ($errors->any())
+	<ul class="alert alert-danger">
+	  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+		@foreach ($errors->all() as $error)
+			<li> {{ $error }} </li>
+		@endforeach
+	</ul>
+@endif
---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="col-sm-offset-2 col-sm-8">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    New Task
+                </div>
+
+                <div class="panel-body">
+                    <!-- Display Validation Errors -->
+                    @include('errors.list')
+
+                    <!-- New Task Form -->
+                    
+                    {!!Form::open(array('url' => '/task', 'class' => 'form-horizontal'))!!}
+
+                        <!-- Task Name -->
+                        <div class="form-group">
+                            {!!Form::label('task-name', 'Task', ['class' => 'col-sm-3 control-label'])!!}
+                           
+
+                            <div class="col-sm-6">
+                                {!!Form::text('name', old('task'), ['class' => 'form-control', 'id' => 'task-name', 'required'])!!}
+                            </div>
+                        </div>
+
+                        <!-- Add Task Button -->
+                        <div class="form-group">
+                            <div class="col-sm-offset-3 col-sm-6">
+                                {!!Form::submit('Add Task', ['class' => 'btn btn-default'])!!}
+                            </div>
+                        </div>
+                    {!! Form::close() !!}
+                </div>
+            </div>
+
+            <!-- Current Tasks -->
+            @if (count($tasks) > 0)
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        Current Tasks
+                    </div>
+
+                    <div class="panel-body">
+                        <table class="table table-striped task-table">
+                            <thead>
+                                <th>Task</th>
+                                <th>&nbsp;</th>
+                            </thead>
+                            <tbody>
+                                @foreach ($tasks as $task)
+                                    <tr>
+                                        <td class="table-text"><div>{{ $task->name }}</div></td>
+
+                                        <!-- Task Delete Button -->
+                                        <td>
+                                            {!!Form::open(['method' => 'DELETE', 'url' => '/task/'.$task->id])!!}
+
+                                                {!!Form::submit('Delete',['id' => 'delete-task-'.$task->id, 'class' => 'btn btn-danger' ])!!}
+                                                
+                                            {!! Form::close() !!}
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            @endif
+        </div>
+    </div>
+@endsection
